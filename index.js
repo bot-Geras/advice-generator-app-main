c371e7fb3b2f6e8524592032c66f85c14f9f4c3c
Brian <briandave771@gmail.com>
2022-11-25 12:21:34 +0300

    Refactor the body elements

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const GET_URL = "https://api.adviceslip.com/advice"
 function getAdvice() {
-  fetch("https://api.adviceslip.com/advice")
+  fetch(GET_URL)
     .then((Response) => Response.json())
     .then((data) => {
       //console.log(data);
+      const {id, advice} = data.slip
       document.getElementById(
         "card-title"
-      ).textContent = `Advice # ${data.slip.id}`;
-      document.getElementById("card-body").textContent = `${data.slip.advice}`;
+      ).textContent = `Advice # ${id}`;
+      document.getElementById("card-body").textContent = `${advice}`;
     });
 }
 
